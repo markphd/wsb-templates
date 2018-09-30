08617785cfaa7e9765d1c95ac9e364524fa129ff
Mark <radical.mark@gmail.com>
2018-09-30 14:20:58 +0400

    fix db duplicate entries, add template list to state, render template data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/db.json'
+
 
 class App extends Component {
+  componentWillMount() {
+    const Datastore = require('nedb')
+    const db = new Datastore({})  
+    var doc = data.templates;
+
+    doc.map(template => db.insert(template, (err, templates) => err))
+
+    db.find({}, (err, templates) => this.setState( (state) => state = {templates} ))
+  }
+
+
+  componentDidMount() {
+    console.log("mounted")
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      templates: [{}]
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +35,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.templates.map( (item) => {
+            return <p>{item.name}, {item.id}</p>
+          }) }
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import data from './data/db.json'
 
-const Datastore = require('nedb')
-const db = new Datastore({});
-
-var doc = data.templates;
-
-doc.map(template => db.insert(template, (err, docs) => err))
-
-db.find({'categories': ['food_and_hospitality']}, function(err, docs) {
-  console.log(docs)
-})
-
-db.find({'name': 'Family Album'}, function(err, docs) {
-  console.log(docs)
-})
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App  />, document.getElementById('root'));
 registerServiceWorker();
