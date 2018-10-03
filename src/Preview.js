756da3fe920cb7205feffa8769a9a287dd944ac3
Mark Philip Dawal <radical.mark@gmail.com>
2018-10-03 22:33:06 +0400

    TODO: create preview component

--- src/Preview.js
+++ src/Preview.js
@@ -1,1 +1,32 @@
-Preview.js
+import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import templateOrder from './TemplateOrder';
+
+const find = id => templateOrder.find(p => p.id == id);
+
+const Person = ({ match }) => {
+  const person = find(match.params.id);
+
+  return (
+    <div>
+      <h3>{person.name}’s Friends</h3>
+      <ul>
+        {person.friends.map(id => (
+          <li key={id}>
+            <Link to={`${match.url}/${id}`}>{find(id).name}</Link>
+          </li>
+        ))}
+      </ul>
+      <Route path={`${match.url}/:id`} component={Person} />
+    </div>
+  );
+};
+
+export class Preview extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
